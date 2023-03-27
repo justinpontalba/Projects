ca6a0b0baa84ff9e76effb63d0359eeb44d7b902
Justin Pontalba <jpontalba@novacene.ai>
2023-03-26 20:54:46 -0400

    feat: add celery and flower

--- django-real-estate/estate-src/real_estate/settings/development.py
+++ django-real-estate/estate-src/real_estate/settings/development.py
@@ -3,7 +3,7 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND= "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST=env("EMAIL_HOST")
 EMAIL_USER_TLS=True
 EMAIL_PORT=env("EMAIL_PORT")
@@ -25,6 +25,10 @@ DATABASES = {
         "PORT": env("PG_PORT"),
     }
 }
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = env("CELERY_BACKEND")
+CELERY_TIMEZONE = "Africa/Kigali"
 
 
 # POSTGRES_ENGINE=django.db.backends.postgresql
